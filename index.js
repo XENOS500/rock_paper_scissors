88c49a1adbaa2cd811f5449a0986c756e357fd50
XENOS500 <231503629+XENOS500@users.noreply.github.com>
2026-07-22 18:08:09 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ index.js
@@ -0,0 +1,51 @@
+//ROCK PAPER SCISSORS
+
+const choices =["Rock","Paper","Scissors"]
+const playerDisplay = document.getElementById("PlayerDisplay");
+const computerDisplay = document.getElementById("ComputerDisplay");
+
+const resultDisplay = document.getElementById("ResultDisplay")
+const playerScoreDisplay =document.getElementById("playerScoreDisplay")
+const computerScoreDisplay =document.getElementById("computerScoreDisplay")
+let playerScore =0;
+let computerScore =0;
+function playGame(playerChoice)
+{
+    const computerChoice = choices[Math.floor(Math.random()*3)]
+    let result=""
+    if(playerChoice==computerChoice)
+    {
+        result="IT'S A TIE!"
+    }
+    else {
+        switch(playerChoice)
+        {
+            case "Rock":
+                result=(computerChoice==="Scissors")?"YOU WIN!" :"YOU LOSE";
+                break;
+            case "Paper":
+                result=(computerChoice==="Rock")?"YOU WIN!":"YOU LOSE";
+                break;
+            case "Scissors":
+                result=(computerChoice==="Paper")?"YOU WIN!":"YOU LOSE";
+                break;
+        }
+    }
+    playerDisplay.textContent = `PLAYER : ${playerChoice}`;
+    computerDisplay.textContent = `COMPUTER : ${computerChoice}`;
+    resultDisplay.textContent =result;
+    resultDisplay.classList.remove("greenText","redText")
+    switch(result)
+    { case "YOU WIN!":
+        resultDisplay.classList.add("greenText");
+        playerScore++;
+         playerScoreDisplay.textContent=playerScore;
+        break;
+      case "YOU LOSE":
+        resultDisplay.classList.add("redText");
+        computerScore++;
+        computerScoreDisplay.textContent=computerScore;
+
+        break;
+    }
+}
